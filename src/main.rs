a78562e4107e12585bc89fa15e33ec5314a94849
David Zhao <taipan.snake13@gmail.com>
2019-01-30 01:16:28 +1100

    write and test some file i/o so i can serve a static html page

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+use actix_web::{server, App, http, HttpRequest, HttpResponse};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
-fn index(_req: &HttpRequest) -> &'static str {
-    return "Hello";
+fn readFromFile(fileName: &'static str) -> std::io::Result<String> {
+    // read from file
+    let file = File::open(fileName)?;
+    let mut bufReader = BufReader::new(file);
+
+    let mut fileContents = String::new();
+    bufReader.read_to_string(&mut fileContents)?;
+
+    Ok(fileContents)
+}
+
+
+
+fn index(_req: &HttpRequest) -> HttpResponse {
+    // let s = "<!DOCTYPE html>\n<html>\n<head><title>LOLOL</title></head><body>\n<b>Hello</b>\n</body>\n</html>";
+    let s = match readFromFile("test.html") {
+        Ok(s) => s,
+        e => String::from("Error :("),
+    };
+    
+    return HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "text/html")
+        .body(s);
 }
 
 fn main() {
